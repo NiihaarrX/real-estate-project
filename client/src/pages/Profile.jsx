6660c994a247760d511e4910d95820f049021516
Nihar Popat <niharpopat17@gmail.com>
2024-09-20 22:58:31 +0530

    Update listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -115,6 +115,27 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -199,7 +220,8 @@ export default function Profile() {
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>
+                <button className='text-red-700 uppercase'
+                onClick={() => handleListingDelete(listing._id)}>
                   Delete
                 </button>
                 <Link to={`/update-listing/${listing._id}`}>
